74a04b5ecbc3604941b376c2c4c535e3af5a1a9a
Abdallah Awwad <ar.awwad6@gmail.com>
2025-02-09 15:10:51 +0100

    add thread safety for db operations with mutex

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/internal/resp/types"
@@ -16,38 +17,46 @@ func (val *dbValue) hasExpired() bool {
 	return !val.expires.IsZero() && time.Now().After(val.expires)
 }
 
-type db map[types.RespType]dbValue
-
-func New() db {
-	return db{}
+type db struct {
+	values map[types.RespType]dbValue
+	mu     *sync.RWMutex
 }
 
-func (db db) Set(key types.RespType, value types.RespType, ttl *time.Duration) {
+func New() db {
+	return db{values: map[types.RespType]dbValue{}, mu: &sync.RWMutex{}}
+}
+
+func (db *db) Set(key types.RespType, value types.RespType, ttl *time.Duration) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	expires := time.Time{}
 	if ttl != nil {
 		expires = time.Now().Add(*ttl)
 	}
-	db[key] = dbValue{
+	db.values[key] = dbValue{
 		value:   value,
 		expires: expires,
 	}
 }
 
-func (db db) Get(key types.RespType) (types.RespType, bool) {
-	val, ok := db[key]
+func (db *db) Get(key types.RespType) (types.RespType, bool) {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	val, ok := db.values[key]
 	if !ok {
 		return nil, false
 	}
 
 	if val.hasExpired() {
-		delete(db, key)
+		delete(db.values, key)
 		return nil, false
 	}
 
 	return val.value, true
 }
 
-func (db db) Incr(key types.RespType) types.RespType {
+func (db *db) Incr(key types.RespType) types.RespType {
 	val, ok := db.Get(key)
 	if !ok {
 		db.Set(key, types.BulkString("1"), nil)
